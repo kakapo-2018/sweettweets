2e744b0a24f341b1396e2ea040a44c9cd63b86e9
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-09-24 15:11:01 +1200

    corrected typo

--- client/components/TweetList.jsx
+++ client/components/TweetList.jsx
@@ -33,4 +33,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Tweetlist);
+export default connect(mapStateToProps, mapDispatchToProps)(TweetList);
