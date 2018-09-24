d76c02cbffffb83fede28eec72a5655515d1c150
harrison-symes <zanelockett@gmail.com>
2018-09-25 09:44:55 +1200

    logo

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -41,6 +41,7 @@ const Navbar = ({
               </button>
             </div>
           )}
+          {/* <img src="/images/logo/EDA-logo.png"/> */}
           <hr />
         </div>
       </div>
