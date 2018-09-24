59502ac45d48e4a07ff077ce164ba64699ad5d50
harrison-symes <zanelockett@gmail.com>
2018-09-25 10:57:06 +1200

    tweet for seeds

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,84 +1,148 @@
 import React from "react";
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
 import Navbar from "./Navbar";
-// import ErrorMessage from "./ErrorMessage"
-// import Links from "./Links"
-// import Loading from "./Loading"
-import LoginForm from "./LoginForm"
-// import Logout from "./Logout"
-import RegisterForm from "./RegisterForm"
-import TweetList from "./TweetList"
-
-import { getTweet } from './../actions/tweet'
-// import { requestLogin, receiveLogin, loginError } from "../actions/login"
-// import { requestLogout, receiveLogout } from "../actions/logout"
-// import { requestRegister, registerError, registerUser } from "../actions/register"
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+import TweetList from "./TweetList";
+import AddTweet from "./AddTweet";
+import { getTweet } from "./../actions/tweet";
+import ProfileForm from "./ProfileForm";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       showRegister: false,
       showLogin: false,
-      showTweets: true
-    }
-    this.loginClick = this.loginClick.bind(this)
-    this.registerClick = this.registerClick.bind(this)
+      showTweets: true,
+      showProfile: false,
+      addTweet: false
+    };
+    this.loginClick = this.loginClick.bind(this);
+    this.toggleLogin = this.toggleLogin.bind(this);
+    this.registerClick = this.registerClick.bind(this);
+    this.profileClick = this.profileClick.bind(this);
+    this.tweetUpdate = this.tweetUpdate.bind(this);
+    this.toggleRegister = this.toggleRegister.bind(this);
+    this.addClick = this.addClick.bind(this);
   }
 
   componentDidMount() {
-    this.props.getTweet()
+    this.props.getTweet();
+  }
+
+  addClick() {
+    this.setState(prevState => ({
+      addTweet: !prevState.addTweet
+    }));
+  }
+
+  profileClick() {
+    if (!this.state.showProfile && !this.state.showLogin) {
+      return this.setState({ showProfile: true });
+    } else {
+      return this.setState({ showProfile: false });
+    }
   }
 
   loginClick() {
-    // console.log('clicked')
     if (!this.state.showLogin) {
-      return this.setState({ showLogin: true, showRegister: false });
+      return this.setState({
+        showLogin: true,
+        showRegister: false,
+        showProfile: false
+      });
     } else {
-      return this.setState({ showLogin: false, showRegister: false });
+      return this.setState({
+        showLogin: false,
+        showRegister: false,
+        showProfile: false
+      });
     }
-
   }
 
   registerClick() {
-    // console.log('clickedreg')
     if (!this.state.showRegister) {
-      return this.setState({ showRegister: true, showLogin: false });
+      return this.setState({
+        showRegister: true,
+        showLogin: false,
+        showProfile: false
+      });
     } else {
-      return this.setState({ showLogin: false, showRegister: false })
+      return this.setState({
+        showLogin: false,
+        showRegister: false,
+        showProfile: false
+      });
     }
+  }
 
+  tweetUpdate() {
+    this.props.getTweet();
+  }
+
+  toggleRegister() {
+    this.setState({
+      showRegister: false,
+      showLogin: false,
+      showTweets: true
+    });
+  }
+
+  toggleLogin() {
+    this.setState({
+      showRegister: false,
+      showLogin: false,
+      showTweets: true
+    });
   }
 
   render() {
     return (
+      <div id="body2">
+        <h1 id="titleHead">Sweet Tweets <img src="/images/logo/EDA-logo.png" id="logo"/>
+</h1>
 
-      <div>
-        <h1>Sweet Tweets</h1>
-        <Navbar loginClick={this.loginClick} registerClick={this.registerClick} />
-        {this.state.showLogin && <LoginForm />}
-        {this.state.showRegister && <RegisterForm />}
+        <Navbar
+          loginClick={this.loginClick}
+          registerClick={this.registerClick}
+          profileClick={this.profileClick}
+          addClick={this.addClick}
+        />
+        {this.state.showLogin && <LoginForm toggleLogin={this.toggleLogin} />}
+        {this.state.showRegister && (
+          <RegisterForm toggleRegister={this.toggleRegister} />
+        )}
+        {this.state.showProfile &&
+          this.props.auth.isAuthenticated && <ProfileForm />}
+
+        {this.state.addTweet &&
+          this.props.auth.isAuthenticated && (
+            <AddTweet tweetUpdate={this.tweetUpdate} />
+          )}
         {this.state.showTweets && <TweetList />}
       </div>
-
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     auth: state.auth,
     tweets: state.tweets
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     getTweet: () => {
-      dispatch(getTweet())
+      dispatch(getTweet());
     }
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
