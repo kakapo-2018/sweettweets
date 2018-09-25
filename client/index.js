f9b25c62db79c1b6ad6b7edf24aaddf610e660d9
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-25 20:58:27 +1200

    firefox now works

--- client/index.js
+++ client/index.js
@@ -10,7 +10,6 @@ import App from './components/App'
 
 const store = createStore(reducers, compose(
   applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 document.addEventListener('DOMContentLoaded', () => {
